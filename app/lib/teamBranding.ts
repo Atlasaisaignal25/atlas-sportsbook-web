f4b4a38a0e6766cfe4e522de7f53c0c137fbefb0
Falcon TPK <falcontpk@Falcons-MacBook.local>
2026-04-13 17:05:05 -0400

    Add soccer team branding entries

--- app/lib/teamBranding.ts
+++ app/lib/teamBranding.ts
@@ -161,4 +161,694 @@ export const teamBranding: Record<
     abbr: "WAS",
     logo: "/team-logos/nba/wizards.png",
   },
-};
+
+"Anaheim Ducks": {
+    shortName: "Ducks",
+    abbr: "ANA",
+    logo: "/team-logos/nhl/anaheimducks.png",
+  },
+  "Boston Bruins": {
+    shortName: "Bruins",
+    abbr: "BOS",
+    logo: "/team-logos/nhl/bostonbruins.png",
+  },
+  "Buffalo Sabres": {
+    shortName: "Sabres",
+    abbr: "BUF",
+    logo: "/team-logos/nhl/buffalosabres.png",
+  },
+  "Calgary Flames": {
+    shortName: "Flames",
+    abbr: "CGY",
+    logo: "/team-logos/nhl/calgaryflames.png",
+  },
+  "Carolina Hurricanes": {
+    shortName: "Hurricanes",
+    abbr: "CAR",
+    logo: "/team-logos/nhl/carolinahurricanes.png",
+  },
+  "Chicago Blackhawks": {
+    shortName: "Blackhawks",
+    abbr: "CHI",
+    logo: "/team-logos/nhl/chicagoblackhawks.png",
+  },
+  "Colorado Avalanche": {
+    shortName: "Avalanche",
+    abbr: "COL",
+    logo: "/team-logos/nhl/coloradoavalanche.png",
+  },
+  "Columbus Blue Jackets": {
+    shortName: "Blue Jackets",
+    abbr: "CBJ",
+    logo: "/team-logos/nhl/columbusbluejackets.png",
+  },
+  "Dallas Stars": {
+    shortName: "Stars",
+    abbr: "DAL",
+    logo: "/team-logos/nhl/dallasstars.png",
+  },
+  "Detroit Red Wings": {
+    shortName: "Red Wings",
+    abbr: "DET",
+    logo: "/team-logos/nhl/detroitredwings.png",
+  },
+  "Edmonton Oilers": {
+    shortName: "Oilers",
+    abbr: "EDM",
+    logo: "/team-logos/nhl/edmontonoilers.png",
+  },
+  "Florida Panthers": {
+    shortName: "Panthers",
+    abbr: "FLA",
+    logo: "/team-logos/nhl/floridapanthers.png",
+  },
+  "Los Angeles Kings": {
+    shortName: "Kings",
+    abbr: "LAK",
+    logo: "/team-logos/nhl/losangeleskings.png",
+  },
+  "Minnesota Wild": {
+    shortName: "Wild",
+    abbr: "MIN",
+    logo: "/team-logos/nhl/minnesotawild.png",
+  },
+  "Montreal Canadiens": {
+    shortName: "Canadiens",
+    abbr: "MTL",
+    logo: "/team-logos/nhl/montrealcanadiens.png",
+  },
+  "Nashville Predators": {
+    shortName: "Predators",
+    abbr: "NSH",
+    logo: "/team-logos/nhl/nashvillepredators.png",
+  },
+  "New Jersey Devils": {
+    shortName: "Devils",
+    abbr: "NJD",
+    logo: "/team-logos/nhl/newjerseydevils.png",
+  },
+  "New York Islanders": {
+    shortName: "Islanders",
+    abbr: "NYI",
+    logo: "/team-logos/nhl/newyorkislanders.png",
+  },
+  "New York Rangers": {
+    shortName: "Rangers",
+    abbr: "NYR",
+    logo: "/team-logos/nhl/newyorkrangers.png",
+  },
+  "Ottawa Senators": {
+    shortName: "Senators",
+    abbr: "OTT",
+    logo: "/team-logos/nhl/ottawasenators.png",
+  },
+  "Philadelphia Flyers": {
+    shortName: "Flyers",
+    abbr: "PHI",
+    logo: "/team-logos/nhl/philadelphiaflyers.png",
+  },
+  "Pittsburgh Penguins": {
+    shortName: "Penguins",
+    abbr: "PIT",
+    logo: "/team-logos/nhl/pittsburghpenguins.png",
+  },
+  "San Jose Sharks": {
+    shortName: "Sharks",
+    abbr: "SJS",
+    logo: "/team-logos/nhl/sanjosesharks.png",
+  },
+  "Seattle Kraken": {
+    shortName: "Kraken",
+    abbr: "SEA",
+    logo: "/team-logos/nhl/seattlekraken.png",
+  },
+  "St. Louis Blues": {
+    shortName: "Blues",
+    abbr: "STL",
+    logo: "/team-logos/nhl/stlouisblues.png",
+  },
+  "Tampa Bay Lightning": {
+    shortName: "Lightning",
+    abbr: "TBL",
+    logo: "/team-logos/nhl/tampabaylightning.png",
+  },
+  "Toronto Maple Leafs": {
+    shortName: "Maple Leafs",
+    abbr: "TOR",
+    logo: "/team-logos/nhl/torontomapleleafs.png",
+  },
+  "Utah Hockey Club": {
+    shortName: "Utah",
+    abbr: "UTA",
+    logo: "/team-logos/nhl/utahhockeyclub.png",
+  },
+  "Vancouver Canucks": {
+    shortName: "Canucks",
+    abbr: "VAN",
+    logo: "/team-logos/nhl/vancouvercanucks.png",
+  },
+  "Vegas Golden Knights": {
+    shortName: "Golden Knights",
+    abbr: "VGK",
+    logo: "/team-logos/nhl/vegasgoldenknights.png",
+  },
+  "Washington Capitals": {
+    shortName: "Capitals",
+    abbr: "WSH",
+    logo: "/team-logos/nhl/washingtoncapitals.png",
+  },
+  "Winnipeg Jets": {
+    shortName: "Jets",
+    abbr: "WPG",
+    logo: "/team-logos/nhl/winnipegjets.png",
+  },
+
+  "Arizona Diamondbacks": {
+    shortName: "Diamondbacks",
+    abbr: "ARI",
+    logo: "/team-logos/mlb/arizonadiamondbacks.png",
+  },
+  "Atlanta Braves": {
+    shortName: "Braves",
+    abbr: "ATL",
+    logo: "/team-logos/mlb/atlantabraves.png",
+  },
+  "Baltimore Orioles": {
+    shortName: "Orioles",
+    abbr: "BAL",
+    logo: "/team-logos/mlb/baltimoreorioles.png",
+  },
+  "Boston Red Sox": {
+    shortName: "Red Sox",
+    abbr: "BOS",
+    logo: "/team-logos/mlb/bostonredsox.png",
+  },
+  "Chicago Cubs": {
+    shortName: "Cubs",
+    abbr: "CHC",
+    logo: "/team-logos/mlb/chicagocubs.png",
+  },
+  "Chicago White Sox": {
+    shortName: "White Sox",
+    abbr: "CWS",
+    logo: "/team-logos/mlb/chicagowhitesox.png",
+  },
+  "Cincinnati Reds": {
+    shortName: "Reds",
+    abbr: "CIN",
+    logo: "/team-logos/mlb/cincinnatireds.png",
+  },
+  "Cleveland Guardians": {
+    shortName: "Guardians",
+    abbr: "CLE",
+    logo: "/team-logos/mlb/clevelandguardians.png",
+  },
+  "Colorado Rockies": {
+    shortName: "Rockies",
+    abbr: "COL",
+    logo: "/team-logos/mlb/coloradorockies.png",
+  },
+  "Detroit Tigers": {
+    shortName: "Tigers",
+    abbr: "DET",
+    logo: "/team-logos/mlb/detroittigers.png",
+  },
+  "Houston Astros": {
+    shortName: "Astros",
+    abbr: "HOU",
+    logo: "/team-logos/mlb/houstonastros.png",
+  },
+  "Kansas City Royals": {
+    shortName: "Royals",
+    abbr: "KC",
+    logo: "/team-logos/mlb/kansascityroyals.png",
+  },
+  "Los Angeles Angels": {
+    shortName: "Angels",
+    abbr: "LAA",
+    logo: "/team-logos/mlb/losangelesangels.png",
+  },
+  "Los Angeles Dodgers": {
+    shortName: "Dodgers",
+    abbr: "LAD",
+    logo: "/team-logos/mlb/losangelesdodgers.png",
+  },
+  "Miami Marlins": {
+    shortName: "Marlins",
+    abbr: "MIA",
+    logo: "/team-logos/mlb/miamimarlins.png",
+  },
+  "Milwaukee Brewers": {
+    shortName: "Brewers",
+    abbr: "MIL",
+    logo: "/team-logos/mlb/milwaukeebrewers.png",
+  },
+  "Minnesota Twins": {
+    shortName: "Twins",
+    abbr: "MIN",
+    logo: "/team-logos/mlb/minnesotatwins.png",
+  },
+  "New York Mets": {
+    shortName: "Mets",
+    abbr: "NYM",
+    logo: "/team-logos/mlb/newyorkmets.png",
+  },
+  "New York Yankees": {
+    shortName: "Yankees",
+    abbr: "NYY",
+    logo: "/team-logos/mlb/newyorkyankees.png",
+  },
+  "Oakland Athletics": {
+    shortName: "Athletics",
+    abbr: "ATH",
+    logo: "/team-logos/mlb/oaklandathletics.png",
+  },
+  "Philadelphia Phillies": {
+    shortName: "Phillies",
+    abbr: "PHI",
+    logo: "/team-logos/mlb/philadelphiaphillies.png",
+  },
+  "Pittsburgh Pirates": {
+    shortName: "Pirates",
+    abbr: "PIT",
+    logo: "/team-logos/mlb/pittsburghpirates.png",
+  },
+  "San Diego Padres": {
+    shortName: "Padres",
+    abbr: "SD",
+    logo: "/team-logos/mlb/sandiegopadres.png",
+  },
+  "San Francisco Giants": {
+    shortName: "Giants",
+    abbr: "SF",
+    logo: "/team-logos/mlb/sanfranciscogiants.png",
+  },
+  "Seattle Mariners": {
+    shortName: "Mariners",
+    abbr: "SEA",
+    logo: "/team-logos/mlb/seattlemariners.png",
+  },
+  "St. Louis Cardinals": {
+    shortName: "Cardinals",
+    abbr: "STL",
+    logo: "/team-logos/mlb/stlouiscardinals.png",
+  },
+  "Tampa Bay Rays": {
+    shortName: "Rays",
+    abbr: "TB",
+    logo: "/team-logos/mlb/tampabayrays.png",
+  },
+  "Texas Rangers": {
+    shortName: "Rangers",
+    abbr: "TEX",
+    logo: "/team-logos/mlb/texasrangers.png",
+  },
+  "Toronto Blue Jays": {
+    shortName: "Blue Jays",
+    abbr: "TOR",
+    logo: "/team-logos/mlb/torontobluejays.png",
+  },
+  "Washington Nationals": {
+    shortName: "Nationals",
+    abbr: "WSH",
+    logo: "/team-logos/mlb/washingtonnationals.png",
+  },
+
+  "Arsenal": {
+    shortName: "Arsenal",
+    abbr: "ARS",
+    logo: "/team-logos/soccer/arsenal.png",
+  },
+  "Aston Villa": {
+    shortName: "Aston Villa",
+    abbr: "AVL",
+    logo: "/team-logos/soccer/astonvilla.png",
+  },
+  "Bournemouth": {
+    shortName: "Bournemouth",
+    abbr: "BOU",
+    logo: "/team-logos/soccer/bournemouth.png",
+  },
+  "Brentford": {
+    shortName: "Brentford",
+    abbr: "BRE",
+    logo: "/team-logos/soccer/brentford.png",
+  },
+  "Brighton and Hove Albion": {
+    shortName: "Brighton",
+    abbr: "BHA",
+    logo: "/team-logos/soccer/brightonandhovealbion.png",
+  },
+  "Brighton & Hove Albion": {
+    shortName: "Brighton",
+    abbr: "BHA",
+    logo: "/team-logos/soccer/brightonandhovealbion.png",
+  },
+  "Chelsea": {
+    shortName: "Chelsea",
+    abbr: "CHE",
+    logo: "/team-logos/soccer/chelsea.png",
+  },
+  "Crystal Palace": {
+    shortName: "Crystal Palace",
+    abbr: "CRY",
+    logo: "/team-logos/soccer/crystalpalace.png",
+  },
+  "Everton": {
+    shortName: "Everton",
+    abbr: "EVE",
+    logo: "/team-logos/soccer/everton.png",
+  },
+  "Fulham": {
+    shortName: "Fulham",
+    abbr: "FUL",
+    logo: "/team-logos/soccer/fulham.png",
+  },
+  "Ipswich Town": {
+    shortName: "Ipswich",
+    abbr: "IPS",
+    logo: "/team-logos/soccer/ipswichtown.png",
+  },
+  "Leicester City": {
+    shortName: "Leicester",
+    abbr: "LEI",
+    logo: "/team-logos/soccer/leicestercity.png",
+  },
+  "Liverpool": {
+    shortName: "Liverpool",
+    abbr: "LIV",
+    logo: "/team-logos/soccer/liverpool.png",
+  },
+  "Manchester City": {
+    shortName: "Man City",
+    abbr: "MCI",
+    logo: "/team-logos/soccer/manchestercity.png",
+  },
+  "Manchester United": {
+    shortName: "Man United",
+    abbr: "MUN",
+    logo: "/team-logos/soccer/manchesterunited.png",
+  },
+  "Newcastle United": {
+    shortName: "Newcastle",
+    abbr: "NEW",
+    logo: "/team-logos/soccer/newcastleunited.png",
+  },
+  "Nottingham Forest": {
+    shortName: "Nottm Forest",
+    abbr: "NFO",
+    logo: "/team-logos/soccer/nottinghamforest.png",
+  },
+  "Southampton": {
+    shortName: "Southampton",
+    abbr: "SOU",
+    logo: "/team-logos/soccer/southampton.png",
+  },
+  "Tottenham Hotspur": {
+    shortName: "Tottenham",
+    abbr: "TOT",
+    logo: "/team-logos/soccer/tottenhamhotspur.png",
+  },
+  "West Ham United": {
+    shortName: "West Ham",
+    abbr: "WHU",
+    logo: "/team-logos/soccer/westhamunited.png",
+  },
+  "Wolverhampton Wanderers": {
+    shortName: "Wolves",
+    abbr: "WOL",
+    logo: "/team-logos/soccer/wolverhamptonwanderers.png",
+  },
+
+  "Barcelona": {
+    shortName: "Barcelona",
+    abbr: "BAR",
+    logo: "/team-logos/soccer/barcelona.png",
+  },
+  "Real Madrid": {
+    shortName: "Real Madrid",
+    abbr: "RMA",
+    logo: "/team-logos/soccer/realmadrid.png",
+  },
+  "Atletico Madrid": {
+    shortName: "Atletico",
+    abbr: "ATM",
+    logo: "/team-logos/soccer/atleticomadrid.png",
+  },
+  "Atlético Madrid": {
+    shortName: "Atletico",
+    abbr: "ATM",
+    logo: "/team-logos/soccer/atleticomadrid.png",
+  },
+  "Sevilla": {
+    shortName: "Sevilla",
+    abbr: "SEV",
+    logo: "/team-logos/soccer/sevilla.png",
+  },
+  "Valencia": {
+    shortName: "Valencia",
+    abbr: "VAL",
+    logo: "/team-logos/soccer/valencia.png",
+  },
+  "Villarreal": {
+    shortName: "Villarreal",
+    abbr: "VIL",
+    logo: "/team-logos/soccer/villarreal.png",
+  },
+  "Real Betis": {
+    shortName: "Betis",
+    abbr: "BET",
+    logo: "/team-logos/soccer/realbetis.png",
+  },
+  "Real Sociedad": {
+    shortName: "Sociedad",
+    abbr: "RSO",
+    logo: "/team-logos/soccer/realsociedad.png",
+  },
+  "Athletic Bilbao": {
+    shortName: "Bilbao",
+    abbr: "ATH",
+    logo: "/team-logos/soccer/athleticbilbao.png",
+  },
+  "Getafe": {
+    shortName: "Getafe",
+    abbr: "GET",
+    logo: "/team-logos/soccer/getafe.png",
+  },
+  "Levante": {
+    shortName: "Levante",
+    abbr: "LEV",
+    logo: "/team-logos/soccer/levante.png",
+  },
+  "Leeds United": {
+    shortName: "Leeds",
+    abbr: "LEE",
+    logo: "/team-logos/soccer/leedsunited.png",
+  },
+
+  "Juventus": {
+    shortName: "Juventus",
+    abbr: "JUV",
+    logo: "/team-logos/soccer/juventus.png",
+  },
+  "Inter": {
+    shortName: "Inter",
+    abbr: "INT",
+    logo: "/team-logos/soccer/inter.png",
+  },
+  "Internazionale": {
+    shortName: "Inter",
+    abbr: "INT",
+    logo: "/team-logos/soccer/inter.png",
+  },
+  "AC Milan": {
+    shortName: "Milan",
+    abbr: "MIL",
+    logo: "/team-logos/soccer/acmilan.png",
+  },
+  "Roma": {
+    shortName: "Roma",
+    abbr: "ROM",
+    logo: "/team-logos/soccer/roma.png",
+  },
+  "Lazio": {
+    shortName: "Lazio",
+    abbr: "LAZ",
+    logo: "/team-logos/soccer/lazio.png",
+  },
+  "Napoli": {
+    shortName: "Napoli",
+    abbr: "NAP",
+    logo: "/team-logos/soccer/napoli.png",
+  },
+  "Atalanta": {
+    shortName: "Atalanta",
+    abbr: "ATA",
+    logo: "/team-logos/soccer/atalanta.png",
+  },
+  "Fiorentina": {
+    shortName: "Fiorentina",
+    abbr: "FIO",
+    logo: "/team-logos/soccer/fiorentina.png",
+  },
+  "Bologna": {
+    shortName: "Bologna",
+    abbr: "BOL",
+    logo: "/team-logos/soccer/bologna.png",
+  },
+
+  "Paris Saint-Germain": {
+    shortName: "PSG",
+    abbr: "PSG",
+    logo: "/team-logos/soccer/parissaintgermain.png",
+  },
+  "PSG": {
+    shortName: "PSG",
+    abbr: "PSG",
+    logo: "/team-logos/soccer/parissaintgermain.png",
+  },
+  "Marseille": {
+    shortName: "Marseille",
+    abbr: "MAR",
+    logo: "/team-logos/soccer/marseille.png",
+  },
+  "Monaco": {
+    shortName: "Monaco",
+    abbr: "MON",
+    logo: "/team-logos/soccer/monaco.png",
+  },
+  "Lyon": {
+    shortName: "Lyon",
+    abbr: "LYO",
+    logo: "/team-logos/soccer/lyon.png",
+  },
+
+  "Bayern Munich": {
+    shortName: "Bayern",
+    abbr: "BAY",
+    logo: "/team-logos/soccer/bayernmunich.png",
+  },
+  "Bayer Leverkusen": {
+    shortName: "Leverkusen",
+    abbr: "B04",
+    logo: "/team-logos/soccer/bayerleverkusen.png",
+  },
+  "Borussia Dortmund": {
+    shortName: "Dortmund",
+    abbr: "BVB",
+    logo: "/team-logos/soccer/borussiadortmund.png",
+  },
+  "RB Leipzig": {
+    shortName: "Leipzig",
+    abbr: "RBL",
+    logo: "/team-logos/soccer/rbleipzig.png",
+  },
+
+  "Benfica": {
+    shortName: "Benfica",
+    abbr: "BEN",
+    logo: "/team-logos/soccer/benfica.png",
+  },
+  "Porto": {
+    shortName: "Porto",
+    abbr: "POR",
+    logo: "/team-logos/soccer/porto.png",
+  },
+  "Sporting CP": {
+    shortName: "Sporting",
+    abbr: "SCP",
+    logo: "/team-logos/soccer/sportingcp.png",
+  },
+
+  "Club America": {
+    shortName: "Club America",
+    abbr: "AME",
+    logo: "/team-logos/soccer/clubamerica.png",
+  },
+  "Club América": {
+    shortName: "Club America",
+    abbr: "AME",
+    logo: "/team-logos/soccer/clubamerica.png",
+  },
+  "Chivas Guadalajara": {
+    shortName: "Chivas",
+    abbr: "GDL",
+    logo: "/team-logos/soccer/chivasguadalajara.png",
+  },
+  "Monterrey": {
+    shortName: "Monterrey",
+    abbr: "MTY",
+    logo: "/team-logos/soccer/monterrey.png",
+  },
+  "Tigres UANL": {
+    shortName: "Tigres",
+    abbr: "TIG",
+    logo: "/team-logos/soccer/tigresuanl.png",
+  },
+  "Cruz Azul": {
+    shortName: "Cruz Azul",
+    abbr: "CAZ",
+    logo: "/team-logos/soccer/cruzazul.png",
+  },
+  "Pumas UNAM": {
+    shortName: "Pumas",
+    abbr: "PUM",
+    logo: "/team-logos/soccer/pumasunam.png",
+  },
+
+  "Inter Miami CF": {
+    shortName: "Inter Miami",
+    abbr: "MIA",
+    logo: "/team-logos/soccer/intermiamicf.png",
+  },
+  "LA Galaxy": {
+    shortName: "LA Galaxy",
+    abbr: "LAG",
+    logo: "/team-logos/soccer/lagalaxy.png",
+  },
+  "Los Angeles FC": {
+    shortName: "LAFC",
+    abbr: "LAFC",
+    logo: "/team-logos/soccer/losangelesfc.png",
+  },
+  "New York City FC": {
+    shortName: "NYCFC",
+    abbr: "NYC",
+    logo: "/team-logos/soccer/newyorkcityfc.png",
+  },
+  "Atlanta United": {
+    shortName: "Atlanta Utd",
+    abbr: "ATL",
+    logo: "/team-logos/soccer/atlantaunited.png",
+  },
+  "Seattle Sounders": {
+    shortName: "Sounders",
+    abbr: "SEA",
+    logo: "/team-logos/soccer/seattlesounders.png",
+  },
+  "Seattle Sounders FC": {
+    shortName: "Sounders",
+    abbr: "SEA",
+    logo: "/team-logos/soccer/seattlesounders.png",
+  },
+  "Columbus Crew": {
+    shortName: "Crew",
+    abbr: "CLB",
+    logo: "/team-logos/soccer/columbuscrew.png",
+  },
+  "FC Cincinnati": {
+    shortName: "Cincinnati",
+    abbr: "CIN",
+    logo: "/team-logos/soccer/fccincinnati.png",
+  },
+  "Orlando City SC": {
+    shortName: "Orlando City",
+    abbr: "ORL",
+    logo: "/team-logos/soccer/orlandocitysc.png",
+  },
+  "Philadelphia Union": {
+    shortName: "Union",
+    abbr: "PHI",
+    logo: "/team-logos/soccer/philadelphiaunion.png",
+  },
+}
